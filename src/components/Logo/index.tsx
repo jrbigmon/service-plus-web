5342df3c0dc19412ffa8a50511eb3477179bd292
jrbigmon <juniorterriaga0@gmail.com>
2022-09-26 16:41:12 -0300

    created logo component for mobile screen

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+
+export function LogoMobile(){
+    return (
+        <div className="font-black text-2xl text-cemter w-full flex justify-center p-4 border-b-2 shadow-lg">
+           <a href="">
+                <span className="text-sky-800">
+                    SERVICE
+                </span>
+                <span className="text-sky-700">
+                    PLUS
+                </span>
+           </a>
+        </div>
+    )
+}
